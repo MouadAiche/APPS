13df4d0d145a74f2f911b6718ff9e60f630f04e0
MouadAiche <mouadaiche8@gmail.com>
2025-05-22 16:02:27 +0100

    CM TO INCHE PROJECT

---
+++ CM-TO-INCHE/app.js
@@ -0,0 +1,6 @@
+let submit = document.getElementById("submit").onclick = function (){
+    let inche = Number(document.getElementById("inche").value)
+    let result = inche * 2.54
+    document.getElementById("cm").textContent = result + "CM"
+    console.log(result)
+}
